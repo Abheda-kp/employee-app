99748473ff7802398aabdf1c30e22b08f917c188
Abheda K P <114218012+Abheda-kp@users.noreply.github.com>
2025-06-05 18:55:14 +0530

    Update navbar.tsx

--- src/component/navbar/navbar.tsx
+++ src/component/navbar/navbar.tsx
@@ -1,10 +1,15 @@
 import { useNavigate } from "react-router-dom"
 import "./navbar.css"
+import Button from "../button/button"
 const Navbar=()=>{
     const navigate=useNavigate()
     const handleClick=()=>{
           navigate("/employees/list")
     }
+      const handleLogout = () => {
+        localStorage.setItem("token", "");
+        navigate("/login");
+      };
     return( 
         <>
      
@@ -13,6 +18,14 @@ const Navbar=()=>{
                 <ul>
                   
                    <li onClick={handleClick}> <img src="/icon.svg"></img>Employee List</li>
+                </ul>
+                <ul>
+                    <Button
+              type="button"
+              Text="Logout"
+              className="submit"
+              onClick={handleLogout}
+            />
                 </ul>
             </nav>
         </>
